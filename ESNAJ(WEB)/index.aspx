0cba411b621ec19bd70a96a26d17185fe808aa56
JOSE DE JESUS SANCHEZ AGUILAR <jsanc239@itam.mx>
2016-10-27 20:58:42 -0500

    Creación sitio web

---
+++ ESNAJ(WEB)/index.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="index.aspx.cs" Inherits="index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        Bienvenido a la Escuela Nacional de Ajedrez<br />
+        <br />
+        Correo:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="tbCorreo" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        Password:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="tbPass" runat="server" TextMode="Password"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Label ID="Label1" runat="server" Text=" "></asp:Label>
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" Text="Login Maestros" OnClick="Button1_Click" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="Button2" runat="server" Text="Login Alumnos" OnClick="Button2_Click" />
+        <br />
+    </form>
+</body>
+</html>
---
+++ ESNAJ(WEB)/index.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class index : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected OdbcConnection conectarDB()
+    {
+        OdbcConnection conexion = null;
+        try
+        {
+            String conectar = "Data Source=112SALAS31;Initial Catalog=ESNAJ;User ID=sa;Password=sqladmin";
+            conexion = new OdbcConnection(conectar);
+            conexion.Open();
+        }
+        catch (Exception e)
+        {
+            Console.Write(e.ToString());
+        }
+
+        return conexion;
+    }
+
+
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            String query = "SELECT idAlumno, alumno.nombre, correo, puntosTotales, categoria, escuela.nombre as 'Escuela' FROM alumno INNER JOIN escuela ON alumno.idEscuela = escuela.idEscuela WHERE correo LIKE '" + tbCorreo.Text + "' AND contra = '" + tbPass.Text + "'";
+            OdbcConnection con = conectarDB();
+            OdbcCommand sql = new OdbcCommand(query, con);
+            OdbcDataReader lector = sql.ExecuteReader();
+            if (lector.HasRows)
+            {
+                lector.Read();
+                Session["Nombre"] = lector.GetString(1);
+                Session["Correo"] = lector.GetString(2);
+                Session["Puntos"] = lector.GetInt32(3);
+                Session["Categoria"] = lector.GetString(4);
+                Session["Escuela"] = lector.GetString(5);
+            }
+            else
+                Label1.Text = "Usuario/Contraseña incorrectos";
+            con.Close();
+            lector.Close();
+
+            Response.Redirect("PerfilA.aspx");
+        }
+        catch (Exception ex) {
+            Label1.Text = "Error interno";
+        }
+    }
+
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("PerfilM.aspx");
+    }
+}
